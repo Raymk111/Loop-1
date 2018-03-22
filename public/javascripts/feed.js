1ebe17ac0eadddbccf4e1ec6d4d014207772a848
EOIN ROCKALL( 6-10-2017 12-22 <danu7_re@danu7>
2018-03-22 18:47:18 +0000

    Eoin's lastest changes to feed

--- public/javascripts/feed.js
+++ public/javascripts/feed.js
@@ -1,73 +1,65 @@
 var showPosts = false;
-$(document).ready( 
-function()
-{
+
+$(document).ready( function()
+                  {
   var totalCharacters = 140;
-  
-                      
-    $("#postForm").keyup(function (event)
-{
-      var inputText = event.target.value;
-      $("#charRemaining").html(totalCharacters - inputText.length);
-  });
+  $("#postForm").keyup(function (event)
+	{
+      	   var inputText = event.target.value;
+      	   $("#charRemaining").html(totalCharacters - inputText.length);
+  	});
 });
 
-getComments();
-
-$("#postForm").submit(function (event) 
-    { event.preventDefault(); $.post("/addComment", {
-    comment: event.target.inputPost.value }, function (result) {
-    $("#charRemaining").html(totalCharacters); event.target.reset();
-    getComments();
-}); });
+$("#postForm").submit(function (event) { event.preventDefault(); $.post("/addComment", 
+	{
+		comment: event.target.inputPost.value }, function (result) {
+		$("#charRemaining").html(totalCharacters); event.target.reset();
+	});
+	getComments();
+	});
 
 function getComments(){
-$.get( "/getComments", function( data ) {
-var posts = "";
-for(var i=0; i<data.length; i++) {
-    posts += "<div class='well'><div class='row'><div class='col-xs-9'>"
-            + data[i].comment + "</div><div class='col-xs-3'>" +
-            "<button type='button' name='"+data[i]._id+"' class='btn btn-danger'>" 
-            +"Delete</button></div></div></div></div>";
- }
-
-$( "#feedPosts" ).html( posts );
-// Recursively call getComments every 1 seconds
-$( "#count" ).html(data.length);
-if(!showPosts)
-                    $( "#feedPosts" ).hide();
-                else
-                    $( "#feedPosts" ).show();
-
-setTimeout(getComments,1000); });
+	$.get( "/getComments", function( data ) {
+		var posts = "";
+		for(var i=0; i<data.length; i++) {
+			posts += "<div class='well'><div class='row'><div class='col-xs-9'>"
+			+ data[i].comment + "</div><div class='col-xs-3'>" +
+			"<button type='button' name='"+data[i]._id+"' class='btn btn-danger'>" +"Delete</button></div></div></div></div>";
+ 		}
+		$( "#feedPosts" ).html( posts );
+		$("#count").html(data.length);
+		if(!showPosts)
+		{
+		$("#feedPosts").show();
+		}
+		else
+		{
+		$("#feedPosts").show();
+		}
+		// Recursively call getComments every 4 seconds
+		});
+		setTimeout(getComments,4000);
 }
 
+$("#btn-count").click(function (event) { var options = {};
+if(!showPosts)
+{
+$("#feedPosts").show( "blind", options, 1000);
+showPosts = true; }
+else
+{
+$("#feedPosts").hide( "blind", options, 1000); showPosts = false;
+} });
+
 $("#feedPosts").click(function (event) {
-    console.log(event.target.name); 
-    if(event.target.name)
+    console.log(event.target.name); if(event.target.name)
     {
         $.ajax(
             {
-            url: '/removeComment/' + event.target.name, type: 'DELETE',
+            url: '/deleteComment/' + event.target.name, type: 'DELETE',
                 success: function(result) {
                     getComments(); }
             });
     }
 });
 
-/**
-         * Event handler for when the user deletes a comment
-         */
-        $("#btn-count").click(function (event) {
-            var options = {};
-            if(!showPosts)
-            {
-                $("#feedPosts").show( "blind", options, 1000);
-                showPosts = true;
-            }
-            else
-            {
-                $("#feedPosts").hide( "blind", options, 1000);
-                showPosts = false;
-            }
-        });
