98e43277e33979f717698e7e51d949a7accd0481
[raymk11} <[email address]>
2018-04-05 00:32:32 +0100

    fixed date and time problem by using moment.js module easy formatting to irish time

--- models/comments.js
+++ models/comments.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var moment = require('moment-timezone');
 require('./util');
 
 var commentsSchema = new Schema({
     user_name: {type: String},
     comment: {type: String},
-    date_created: {type: Date, default: new Date()},
+    date_created: {type: Date, default: moment().tz("Europe/Dublin").format() },
     up_votes: {type: Number, default: 0},
     down_votes: {type: Number, default: 0},
     loop: {type: String},
